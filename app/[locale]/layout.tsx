94dcb939b11596a319b159e037fe4a0d79c4b461
pacto <kuba.kowalski21331@gmail.com>
2024-12-14 22:49:58 +0100

    test

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,3 +1,4 @@
+import localFont from "next/font/local";
 import { getMessages, setRequestLocale } from "next-intl/server";
 import { LOCALES } from "@i18n/LOCALES";
 import { NextIntlClientProvider } from "next-intl";
@@ -6,6 +7,12 @@ import Providers from "./providers/Providers";
 export async function generateStaticParams() {
   return LOCALES.map((locale) => ({ locale }));
 }
+
+const fontinSmallcaps = localFont({
+  src: "../public/fonts/Fontin-SmallCaps.ttf",
+  display: "swap",
+  variable: "--font-fontin-small-caps",
+});
 
 export default async function LocaleLayout({
   children,
@@ -18,7 +25,7 @@ export default async function LocaleLayout({
   setRequestLocale(locale);
   const messages = await getMessages();
   return (
-    <html lang={locale}>
+    <html className={fontinSmallcaps.className} lang={locale}>
       <body>
         <NextIntlClientProvider messages={messages}>
           <Providers>{children}</Providers>
